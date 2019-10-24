63be8333f0306e3d749fd99cac3575fefb45efda
brindoro <brindoro@gmail.com>
2019-10-23 23:28:54 -0400

    modifications

--- Week 1/Brian-TaskD1_-_LoadAndDisplayImage.py
+++ Week 1/Brian-TaskD1_-_LoadAndDisplayImage.py
@@ -1,39 +1,29 @@
-#// This is a pseudo code file for Merge Robotics, 2020, Infinite Recharge
-#// This is task D1 - > Load and Display Images
-#// Using Java or Python and OpenCV, write a small bit of code to load an image and display it on your screen.
-#// Recommeded starting points -> https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_tutorials.html
-#// Or -> https://www.tutorialspoint.com/opencv/opencv_reading_images.htm
-#// Pseudo code to do this is below
+# This is a python code file for Merge Robotics, 2020, Infinite Recharge
+# This is task D1 - > Load and Display Images
+# Using Python and OpenCV, write a small bit of code to load an image and display it on your screen.
+# Recommeded starting points -> https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_tutorials.html
+# Pseudo code to do this is below
+# Link to tutorial code found -> https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html#sum-it-up
 
-#// Imports!
-#// Python - import modules of code as required (OpenCV here)
-#// Java - import classes or packages as required (certain ones)
+# Imports!
+# modules of code as required (OpenCV here)
 
 import numpy as np
 import cv2
 from pathlib import Path
 
-#// Java only - instantiate any required classes and main
-#// Java only - load.Library (OpenCV here)  
+# define a string variable for the path to the file
+strImageInput = str(Path(__file__).parent.parent / 'CalibrationImages' / 'Cube01.jpg')
 
-#// define a string variable for the path to the file
-
-strImageInput = str(Path(__file__).parent.parent) + '/CalibrationImages/Cube01.jpg'
-
-#// load a color image using string
-
+# load a color image using string variable
 imgImageInput = cv2.imread(strImageInput)
 
-#// display the color image to screen
+# display the color image to screen
+cv2.imshow('image-windows-title-bar',imgImageInput)
 
-cv2.imshow('image-title-frame',imgImageInput)
-
-#// wait for user input to close
-
+# wait for user input to close
 k = cv2.waitKey(0)
 
-#// cleanup and exit
-
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
+# cleanup and exit
+cv2.destroyAllWindows()
 
