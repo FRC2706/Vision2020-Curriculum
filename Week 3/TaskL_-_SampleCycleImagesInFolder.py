cbcc006c998fdc57917ec0e6595f3e4896250793
RachelLucyshyn <43799943+RachelLucyshyn@users.noreply.github.com>
2019-12-23 14:50:46 -0500

    Task L and Task M
    did L and M

--- Week 3/TaskL_-_SampleCycleImagesInFolder.py
+++ Week 3/TaskL_-_SampleCycleImagesInFolder.py
@@ -5,26 +5,51 @@
 # displaying all the images (png or jpg only, our decision) navigating with up/down
 # arrow keys, and exiting with esc key or q.
 
+
+
+
+#L2 in brian branch can do different masking techniques
+
 # imports
+import numpy as np
+import cv2
+from pathlib import Path
+import os
+import glob
 
 # select folder of interest
+#folder = str(Path(__file__).parent.parent / 'CalibrationImages')
+folder = "/Users/rachellucyshyn/Documents/GitHub/Vision2020-Curriculum/CalibrationImages"
 
-# read file names
-
-# filter file names
+# read and filter file names
+filenames = glob.glob(folder + '/' + '*.[jJ][pP][gG]')
 
 # set index of files
 
+i = 0
+
 # begin loop
+while(True):
+    print('beginning of loop, i =', i)
 
-## read file
+    ## read file
+    img = cv2.imread(filenames[i])
 
-## display files
+    ## display files
+    cv2.imshow('img', img)
 
-## wait for user input
+    ## wait for user input
+    k = cv2.waitKey(0)
+    if (k == 27):
+        break;
+    if k == 97: # user wants to move down list (a)
+        i = (i - 1) % len(filenames) # mod (%) gives the remainder-if you go back down it'll give you the last thing
+    if k == 122: # user wants to move up list (z)
+        i = (i + 1) % len(filenames)
 
-## end of loop
+    print('end of loop, i =', i)
+
+    ## end of loop
 
 # cleanup and end of file
-
-#L2 in brian branch can do different masking techniques
+cv2.destroyAllWindows()
--- Week 4/TaskM_-_SampleMouseClickPixel.py
+++ Week 4/TaskM_-_SampleMouseClickPixel.py
@@ -20,3 +20,66 @@
 # https://numpy.org/devdocs/user/quickstart.html
 
 # copy in task D2, based on ideas in link above change it to use BGR and HSV images
+
+# Imports!
+# Python - import modules of code as required (OpenCV here)
+import cv2
+import numpy as np
+
+# Define string variable for path to file
+strImage = "/Users/rachellucyshyn/Documents/GitHub/Vision2020-Curriculum/CalibrationImages/Cube11.jpg"
+
+# load color image with string
+imgBGRInput = cv2.imread(strImage)
+
+# display color image to screen
+cv2.imshow('Original Image', imgBGRInput) #window-title= what the window says at top
+
+
+# mask image to only show yellow
+
+# Convert BGR to HSV
+imgHVSInput = cv2.cvtColor(imgBGRInput, cv2.COLOR_BGR2HSV)
+
+hue = imgHVSInput[:,:,0]
+sat = imgHVSInput[:,:,1]
+val = imgHVSInput[:,:,2]
+
+
+# Define range of colour in HSV (colour wheel- hue divide by 2 cause python is weird)
+lower_yellow = np.array([28,128,128]) #hue/saturation/value (how much black or white)
+upper_yellow = np.array([32,255,255]) # 255= zero black zero white
+
+# Threshold the HSV image to get only yellow colors
+imgBinaryMask = cv2.inRange(imgHVSInput, lower_yellow, upper_yellow)
+
+# Bitwise-AND mask and original image
+imgColorMask = cv2.bitwise_and(imgHVSInput,imgHVSInput, mask = imgBinaryMask) # frame = OG image
+
+
+# display masked images
+cv2.imshow('HVS',imgHVSInput)
+cv2.imshow('Binary Mask',imgBinaryMask)
+cv2.imshow('ColorMask',imgColorMask)
+    
+cv2.imshow('hue-title',hue)
+cv2.imshow('sat-title',sat)
+cv2.imshow('val-title',val)
+
+def mouseRGB():
+    if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
+        colorsB = image[y,x,0]
+        colorsG = image[y,x,1]
+        colorsR = image[y,x,2]
+        colors = image[y,x]
+        print("Red: ",colorsR)
+        print("Green: ",colorsG)
+        print("Blue: ",colorsB)
+        print("Coordinates of pixel: X: ",x,"Y: ",y)
+
+
+# wait for user input to close
+k = cv2.waitKey(0) #will close when key is pressed
+
+# clean and exit
+cv2.destroyAllWindows()
